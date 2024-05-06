31022fa5873942b5e7eddf04cddb165f5119b5c2
pajtoo <ps4ch@seznam.cz>
2024-05-06 18:54:58 +0200

    Increase z-index of flash message and setting its position

--- src/components/FlashMessage.js
+++ src/components/FlashMessage.js
@@ -1,20 +1,22 @@
 import React, { useState } from "react";
+import "../styles/flashMessageStyle.css"
 
 export const FlashMessage = ({ theme, text }) => {
 
   const [isVisible, setIsVisible] = useState(true);
 
-  const handleClose = () => setIsVisible(true);
+  const handleClose = () => setIsVisible(false);
 
-  return (<>
-    {isVisible && <div className={"sticky-top z-1 alert alert-" + theme}>
+  return (
+  <>
+    {isVisible && <div className={"sticky-top-flashmessage z-5 alert alert-" + theme}>
       <div className="container-fluid">
-        <div className="row justify-content-between">
+        <div className="row justify-content-between align-items-center">
           <div className="col">
             {text}
           </div>
-          <div className="col">
-            <button type="button" className="btn" onClick={handleClose}>x</button>
+          <div className="col text-end">
+            <button type="button" className="btn fw-bold fs-5" onClick={handleClose}>x</button>
           </div>
         </div>
       </div>
